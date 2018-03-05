c97fde36e99e9324c169f9639fc7133cc7c86c5a
Maxim <maxim.kovalenko@together.com>
2018-03-05 14:35:06 +0200

    chrome
    add chomedrvier

--- src/main/java/com/dice/base/BaseTest.java
+++ src/main/java/com/dice/base/BaseTest.java
@@ -1,18 +1,19 @@
 package com.dice.base;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
     protected WebDriver driver;
 
+    @Parameters ({ "browser" })
     @BeforeMethod
-    public void methodSetUp() {
+    public void methodSetUp(String browser) {
         System.out.println("Method set up");
-        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
-        driver = new FirefoxDriver();
+        driver = BrowserFactory.getDriver(browser);
+
     }
 
     @AfterMethod
